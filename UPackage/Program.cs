bc0bae6c838c509bd37f59b54ea7f84aa8a8ffe5
Tomek Wiernik <twiernik@gmail.com>
2015-08-19 08:55:30 +0200

    :pill: Upackage Creates packages.config file based on directory content

---
+++ UPackage/Program.cs
@@ -0,0 +1,55 @@
+﻿using NuGet;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System;
+using System.Reflection;
+
+namespace UPackage
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+			new Program().Run(args);
+		}
+
+		private void Run(string[] args)
+		{
+			var items = GetDirectory(args)
+				.GetDirectories()
+				.SelectMany(d => d.GetFiles("*nupkg"))
+				.Select(file => new ZipPackage(file.FullName))
+				.Select(zp => new { zp.Id, zp.Version })
+				.Select(x => new XElement("package",
+					new XAttribute("id", x.Id),
+					new XAttribute("version", x.Version)))
+				.ToList();
+
+			new XDocument(new XElement("packages", items)).Save(Path.Combine(GetDirectory(args).FullName, "_packages.config"));
+		}
+
+		private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+		{
+			if (args.Name.StartsWith("NuGet.Core"))
+			{
+				return Assembly.Load(EmbadedResourceHelper.GetResourceBytes(Assembly.GetExecutingAssembly(), "UPackage.libs.NuGet.Core.dll"));
+			}
+			else if (args.Name.StartsWith("Microsoft.Web.XmlTransform"))
+			{
+				return Assembly.Load(EmbadedResourceHelper.GetResourceBytes(Assembly.GetExecutingAssembly(), "UPackage.libs.Microsoft.Web.XmlTransform.dll"));
+			}
+			return null;
+		}
+
+		private DirectoryInfo GetDirectory(string[] args)
+		{
+			if (args.Length != 0)
+				return new DirectoryInfo(args[0]);
+			else
+				return new DirectoryInfo(".");
+		}
+	}
+}
---
+++ UPackage/ResourceHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace UPackage
+{
+	public static class EmbadedResourceHelper
+	{
+		public static Stream GetResouceStream(Assembly assembly, string resourceName)
+		{
+			return assembly.GetManifestResourceStream(resourceName);
+		}
+
+		public static byte[] GetResourceBytes(Assembly assembly, string resouceName)
+		{
+			using (var stream = GetResouceStream(assembly, resouceName))
+				return ReadFully(stream);
+		}
+
+		/// <summary>
+		/// Thanks John!
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public static byte[] ReadFully(Stream input)
+		{
+			var buffer = new byte[16 * 1024];
+			using (var ms = new MemoryStream())
+			{
+				int read;
+
+				while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+					ms.Write(buffer, 0, read);
+
+				return ms.ToArray();
+			}
+		}
+
+	}
+}
